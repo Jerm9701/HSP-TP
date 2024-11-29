ac101bc999fa7bb9c1b2537fdffcdfc762060b82
jtran <jeremy.tran56@gmail.com>
2024-11-29 14:44:25 +0100

    working matrix add

--- part1.cu
+++ part1.cu
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <ctime>
-void MatrixInit(int* M, int n, int p) {
-    std::srand(std::time(nullptr)); // Seed the random number generator
+void MatrixInit(float* M, int n, int p) {
+    // Create matrix on CPU with random values in {-1,1}
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < p; ++j) {
-            M[i * p + j] = (std::rand() % 2 == 0) ? -1 : 1; // Randomly assign -1 or 1
+            float randomValue = static_cast<float>(rand()) / RAND_MAX;
+            M[i * p + j] = randomValue *2 -1;
         }
     }
 }
 
+void MatrixPrint(float* M, int n, int p){
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < p; ++j) {
+            printf("%.2f\t",M[i * p + j]);
+        }
+        printf("\n");
+    }
+}
+
+float * MatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < p; ++j) {
+            Mout[i * p + j] =  M1[i*p + j] + M2[i*p + j];
+        }
+    }
+    return Mout;
+}
 
 int main(){
     int n =5;
     int p = 5;
-    int *M;
-    M   = (int*)malloc(sizeof(int) * n*p);
-    MatrixInit(M,n,p);
+    float *M1, *M2, *Mout;
+    M1   = (float*)malloc(sizeof(int) * n*p);
+    M2   = (float*)malloc(sizeof(int) * n*p);
+    Mout   = (float*)malloc(sizeof(int) * n*p);
+    MatrixInit(M1,n,p);
+    MatrixInit(M2,n,p);
+    MatrixAdd(M1,M2,Mout,n,p);
+    MatrixPrint(Mout,n,p);
     exit(EXIT_SUCCESS);
 }
