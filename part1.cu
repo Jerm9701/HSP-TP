6f2d47858a47d1ebf4a216dcc8d46ac852ebabdb
jtran <jeremy.tran56@gmail.com>
2024-12-09 08:59:45 +0100

    weird tanh

--- part1.cu
+++ part1.cu
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <cstdlib> 
 #include <iostream>
-
+#include <math.h>
 __global__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
@@ -76,7 +76,16 @@ __global__ void Conv2D(float* M_data, float* C1_kernel, float* C1_mat, int img_w
 		S1_data[i*S1_sz+j] = sum;
 	}
 }
+__global__ void activation_tanh(float *M,float *M_out, int length, int depth){
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+ 	int j = blockDim.y * blockIdx.y + threadIdx.y;
+    int k = blockDim.z * blockIdx.z + threadIdx.z;
+    int index = k*length*length + j*length + i;
+    if (i<length && j<length & k<<depth){
+        M_out[index] = tanhf(M[index]);
+    }
 
+}
 void initializeMatrix_3D(float* M3, int n, int p, int d) {
     for (int l = 0; l < n; ++l) {
         for (int i = 0; i < p; ++i) {
@@ -232,17 +241,20 @@ int p2(){
     int C1_kernel = 5;
     float *M_data = (float*)malloc(img_w*img_w*sizeof(float));
     float *C1_mat = (float*)malloc(nb_filter*C1_sz*C1_sz*sizeof(float));
+    float *C1_activated = (float*)malloc(nb_filter*C1_sz*sizeof(float));
     float *S1_data = (float*)malloc(nb_filter*S1_sz*S1_sz*sizeof(float));
     float *C1_mat_kernel = (float*)malloc(nb_filter*C1_kernel*C1_kernel*sizeof(float));
     MatrixInit(M_data,img_w,img_w);
     initializeMatrix_3D(C1_mat,nb_filter,C1_sz,C1_sz);
+    initializeMatrix_3D(C1_activated,nb_filter,C1_sz,C1_sz);
     initializeMatrix_3D(S1_data,nb_filter,S1_sz,S1_sz);
     rd_initializeMatrix_3D(C1_mat_kernel,nb_filter,C1_kernel,C1_kernel);
 
     // Device matrices
-    float *d_M_data, *d_C1_mat, *d_S1_data, *d_C1_mat_kernel;
+    float *d_M_data, *d_C1_mat,*d_C1_activated, *d_S1_data, *d_C1_mat_kernel;
     cudaMalloc((void**)&d_M_data, img_w*img_w*sizeof(float));
     cudaMalloc((void**)&d_C1_mat, nb_filter*C1_sz*C1_sz*sizeof(float));
+    cudaMalloc((void**)&d_C1_activated, nb_filter*C1_sz*C1_sz*sizeof(float));
     cudaMalloc((void**)&d_S1_data, nb_filter*S1_sz*S1_sz*sizeof(float));
     cudaMalloc((void**)&d_C1_mat_kernel, nb_filter*C1_kernel*C1_kernel*sizeof(float));
     
@@ -253,10 +265,12 @@ int p2(){
     dim3 gridDim((img_w + blockDim.x - 1) / blockDim.x, (img_w + blockDim.y - 1) / blockDim.y);
 
     Conv2D<<<gridDim,blockDim>>>(d_M_data,d_C1_mat_kernel,d_C1_mat,img_w,nb_filter,C1_kernel);
+    activation_tanh<<<gridDim,blockDim>>>(d_C1_mat,d_C1_activated,C1_sz,nb_filter);
     cudaMemcpy(C1_mat, d_C1_mat, nb_filter*C1_sz*C1_sz*sizeof(float),cudaMemcpyDeviceToHost);
+    cudaMemcpy(C1_activated, d_C1_activated, nb_filter*C1_sz*C1_sz*sizeof(float),cudaMemcpyDeviceToHost);
 
     //dim3 gridDim((C1_sz + blockDim.x - 1) / blockDim.x, (C1_sz + blockDim.y - 1) / blockDim.y);
-    Batch_norm<<<gridDim,blockDim>>>(d_C1_mat,d_S1_data,nb_filter,C1_sz,S1_sz);
+    Batch_norm<<<gridDim,blockDim>>>(d_C1_activated,d_S1_data,nb_filter,C1_sz,S1_sz);
     cudaMemcpy(S1_data, d_S1_data,nb_filter*S1_sz*S1_sz*sizeof(float),cudaMemcpyDeviceToHost);
 
     printf("random init matrix 32x32 -- truncated for display--\n");
@@ -270,14 +284,28 @@ int p2(){
         }
         printf("\n");
     }
+    printf("test tanh activation 6x28x28 --truncated for display-- \n");
+    for (int i = 0; i < C1_sz-8; ++i) {
+        for (int j = 0; j < C1_sz-8; ++j) {
+            printf("%.2f\t",C1_activated[2*C1_sz+i * C1_sz + j]);
+        }
+        printf("\n");
+    }
     printf("test batchnorm matrix 6x14x14 --channel 1--\n");
     MatrixPrint(S1_data,S1_sz,S1_sz);
     // Free device memory
     cudaFree(d_M_data);
     cudaFree(d_C1_mat);
+    cudaFree(d_C1_activated);
     cudaFree(d_S1_data);
     cudaFree(d_C1_mat_kernel);
-
+    
+    // Free Host memory
+    cudaFree(M_data);
+    cudaFree(C1_mat);
+    cudaFree(C1_activated);
+    cudaFree(S1_data);
+    cudaFree(C1_mat_kernel);
     return 0;
 
 }
